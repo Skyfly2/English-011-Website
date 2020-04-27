7ed812fff7f412d929c02845d79bdbee5f7efd7e
Skyfly2 <ashhamrick@comcast.net>
2020-04-26 22:23:48 -0400

    Current Page Edits

--- js/altscripts.js
+++ js/altscripts.js
@@ -2,6 +2,8 @@ function mouseon(text, head, title) {
     document.getElementById(title).style.display = 'none';
     document.getElementById(text).style.display = 'block';
     document.getElementById(head).style.display = 'block';
+    document.getElementById(text).style.opacity = '1';
+    document.getElementById(head).style.opacity = '1';
 }
 function mouseoff(text, head, title) {
     document.getElementById(text).style.display = 'none';
