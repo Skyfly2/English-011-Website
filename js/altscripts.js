a3ea77a501f811576c1066612fa6badb98abb617
Asher Hamrick <ashhamrick2@gmail.com>
2020-05-01 02:19:04 -0400

    Better Template for Benefits Page
    Modals and things

--- js/altscripts.js
+++ js/altscripts.js
@@ -2,8 +2,6 @@ function mouseon(text, head, title) {
     document.getElementById(title).style.display = 'none';
     document.getElementById(text).style.display = 'block';
     document.getElementById(head).style.display = 'block';
-    document.getElementById(text).style.opacity = '1';
-    document.getElementById(head).style.opacity = '1';
 }
 function mouseoff(text, head, title) {
     document.getElementById(text).style.display = 'none';
---
+++ js/benscripts.js
@@ -0,0 +1,6 @@
+function mouseover(name){
+    document.getElementById(name).style.display = 'block';
+}
+function mouseoff(name){
+    document.getElementById(name).style.display = 'none';
+}
